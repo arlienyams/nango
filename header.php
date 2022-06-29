b6d5728308334e3dc6ee5efc125817bfd86af29a
Arlington Nyamukapa <info@quatrohaus.com>
2022-06-29 13:06:55 +0000

    add logo

--- header.php
+++ header.php
@@ -4,8 +4,6 @@
 <head>
     <meta charset="<?php bloginfo('charset') ?>">
     <meta name="viewport" content="width=device-width">
-	<meta name="description" content="OutRisk Underwriting Management Agency is a is a young, vibrant and purely digital insurtech that has ingrained the versatility of cutting edge digital technologies as key drivers of its operating model in a bid to meet and exceed dynamic customer needs."/>
-	<meta name="keywords" content=" OutRisk, insurance, vehicle, motor, cover, personal, quote, property, vehicle insurance, get quote, motor vehicle insurance, OutRisk Insurance, full third party, zinara, zbc radio licenses, comprehensive insurance, personal insurance, business insurance, insurance news, Funeral Cash Plan, Hospital Cash Plan, Household Contents Insurance, Home Owners Insurance, Multi-peril Policy, Professional Indemnity Cover, Office Contents, Motor Traders, Motor Fleet, Plate, Goods in Transit, Group Personal Accident Cover, All Assets">
     <title><?php bloginfo('name'); ?></title>
     <link rel="stylesheet" href="styles.css">
     <link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/./img/favicon.png" />
@@ -16,17 +14,14 @@
 
 <body <?php body_class(); ?>>
 
-    <div class="container-fluid for-header" id="outriskHeader">
+    <div class="container-fluid for-header">
         <div class="container">
             <div class="row">
                 <div class="mst-flex">
                     <div class="logo">
                         <div class="log">
                             <a href="<?php echo home_url(); ?>">
-                                <img class="black-logo" alt="Outrisk Logo" src="<?php echo get_stylesheet_directory_uri(); ?>/css/img/outrisk_black_logo.png" />
-                            </a>
-                            <a href="<?php echo home_url(); ?>">
-                                <img class="color-logo" alt="Outrisk Logo" src="<?php echo get_stylesheet_directory_uri(); ?>/css/img/outrisk_main_logo.png" />
+                                <img class="black-logo" alt="Outrisk Logo" src="<?php echo get_stylesheet_directory_uri(); ?>/css/img/nango_logo.png" />
                             </a>
                         </div>
                     </div>
@@ -64,14 +59,5 @@
 
 
             </div>
-        </div>
-        <div id="search" class="search">
-            <form autocomplete="off" role="search" method="get" class="search-form" action="<?php echo home_url('/'); ?>">
-                <label>
-                    <!-- <span class="screen-reader-text"><?php echo _x('Search for:', 'label') ?></span> -->
-                    <input type="search" class="search-field" placeholder="<?php echo esc_attr_x('What Are You Looking For?', 'placeholder') ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x('Search for:', 'label') ?>" />
-                </label>
-                <!-- <input type="submit" class="search-submit" value="<?php echo esc_attr_x('Search', 'submit button') ?>" /> -->
-            </form>
         </div>
     </div>
